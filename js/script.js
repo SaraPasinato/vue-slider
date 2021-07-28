0cb08de5ea5a202afedc9685bb54b9e6551be3ac
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-28 16:34:04 +0200

    add slider dots

--- js/script.js
+++ js/script.js
@@ -4,26 +4,33 @@
  ?Bonus:
   Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
 */
-console.log("HERE",Vue);
+console.log("HERE", Vue);
 
-Vue.config.devtools=true;
+Vue.config.devtools = true;
 
 
 
 var app = new Vue({
-    el: '#app',
-    data: {
-     current:0,
-     images:[
-       "img/image1.jpg",
-       "img/image2.jpg",
-       "img/image3.jpg",
-       "img/image4.jpg",
-     ],
+  el: '#app',
+  data: {
+    current: 0,
+    images: [
+      "img/image1.jpg",
+      "img/image2.jpg",
+      "img/image3.jpg",
+      "img/image4.jpg",
+    ]
+  },
+  methods: {
+    isActive(index) {
+      return this.current === index ? "active" : " ";
     },
-    methods:{
-      isActive(index){
-        return this.current===index ? "active": " ";
-      }
+    decreaseCurrent() {
+      this.current = (this.current === 0 ? (this.images.length - 1) : (this.current - 1));
+    },
+    
+    increaseCurrent() {
+      this.current = (this.current === (this.images.length - 1) ? 0 : this.current + 1);
     }
-  });
+  }
+});
