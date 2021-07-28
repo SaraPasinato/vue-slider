fe0166a3bbc9088014488df341ff64b7397f1baf
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-28 15:13:42 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*
+ ?Descrizione:
+  Rifare l'esercizio dello slider come fatto assieme in classe.
+ ?Bonus:
+  Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+*/
 console.log("HERE",Vue);
 
 Vue.config.devtools=true;
