ffc6dfd22ceda25c70fb2c5ed0369591d1adec5b
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-28 16:43:54 +0200

    add animation fadeIn

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 /*
  ?Descrizione:
-  Rifare l'esercizio dello slider come fatto assieme in classe.
+ * Rifare l'esercizio dello slider come fatto assieme in classe.
  ?Bonus:
   Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
 */
 console.log("HERE", Vue);
 
 Vue.config.devtools = true;
-
 
 
 var app = new Vue({
